b43e43cb3c56740809f830708e8c45e2d520ace5
JuanTasayco <juantasayco266@gmail.com>
2025-02-21 15:52:42 -0500

    style: login

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 @Component({
   selector: 'app-forgot-password',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.scss'
 })
 export class ForgotPasswordComponent {
+  formBuilder = inject(FormBuilder)
 
+  forgotForm = this.formBuilder.group({
+    email: [null, [Validators.required]]
+  })
 }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  formBuilder = inject(FormBuilder);
 
+  loginForm = this.formBuilder.group({
+    email: [null, [Validators.required, Validators.email]],
+    password: [null, [Validators.required]],
+    remember: [false, []],
+  })
+
+
+  validateErrorForm(controlName: string) {
+    return this.loginForm.get(controlName)?.invalid && this.loginForm.get(controlName)?.touched;
+  }
+
+  login() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+    } else {
+      console.log('Ingresando Correctamente');
+    }
+  }
+
+  getErrors(controlName: string): string {
+    const controlErrors = this.loginForm.get(controlName)?.errors;
+    if (!controlErrors) return "";
+
+    const errorKeys: string = Object.keys(controlErrors)[0];
+    console.log(errorKeys)
+    const errors: any = {
+      required: "Este campo es oligatorio",
+      minlenght: "El dato es muy pequeño",
+      email: "El formato de email no es correcto",
+    }
+
+    return errors[errorKeys];
+  }
 }
