c86540429fac3ff4235c7e4c095db3ceb7711a7f
juantasayco <juantasayco266@gmail.com>
2025-02-20 21:25:53 -0500

    style: login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { DashBoardRoutes } from './dashboard/dashboard.routes';
 
 export const routes: Routes = [
     {
+        path: "login",
+        loadChildren: () => import("./auth/login.routes").then(m => m.LoginRoutes),
+    },
+    {
         path: "dashboard",
         loadChildren: () => import("./dashboard/dashboard.routes").then(m => m.DashBoardRoutes),
     },
     {
         path: "**",
-        redirectTo: "dashboard"
+        redirectTo: "login"
     }
 ];
---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CardComponent } from '../components/card/card.component';
+
+@Component({
+  selector: 'app-login',
+  imports: [CardComponent],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
